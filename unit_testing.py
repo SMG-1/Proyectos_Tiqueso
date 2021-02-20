7c137f5713ffb140d23b0e97f191350c56d51173
SMG-1 <35125339+SMG-1@users.noreply.github.com>
2021-02-19 21:31:23 -0600

    Update unit_testing.py

--- unit_testing.py
+++ unit_testing.py
@@ -7,9 +7,20 @@ from back_end import Application
 import time
 from win32api import GetSystemMetrics
 import pandastable
+from back_end import *
 
-dicte = {'a': 1, 'b':2, 'c':3}
-print(list(dicte.keys()))
+data = generate_testing_data()
+
+df = pd.DataFrame(data)
 
 
 
+"""auto = AutoRegression(data.values, 1)
+preds = auto.fit().predict()
+preds = pd.DataFrame(index=[i for i in range(auto.ar_lags[0], len(preds) + auto.ar_lags[0], 1)], data=preds)
+
+dftot = pd.concat([df, preds], axis=1)"""
+
+auto = AutoRegression(data.values, 1)
+df_tot = auto.fit_predict()
+auto.show_plot()
