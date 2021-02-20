738afd0c880092751c26137f5b3065641f00dbb3
SMG-1 <35125339+SMG-1@users.noreply.github.com>
2021-02-20 14:16:02 -0600

    Update unit_testing.py

--- unit_testing.py
+++ unit_testing.py
@@ -13,14 +13,22 @@ data = generate_testing_data()
 
 df = pd.DataFrame(data)
 
-
-
 """auto = AutoRegression(data.values, 1)
 preds = auto.fit().predict()
 preds = pd.DataFrame(index=[i for i in range(auto.ar_lags[0], len(preds) + auto.ar_lags[0], 1)], data=preds)
 
 dftot = pd.concat([df, preds], axis=1)"""
 
-auto = AutoRegression(data.values, 1)
+auto = AutoRegression(data.values, 4, 50)
 df_tot = auto.fit_predict()
-auto.show_plot()
+# auto.show_plot_fitted()
+test = auto.predict_fwd()
+auto.show_plot_predicted()
+
+
+default_dict = {'AutoReg': {'params': {'lags': 1,
+                                       'trend': 'ct',
+                                       'n_forward': 50},
+                            'possible_values': {'lags': list(range(1, 50, 1)),
+                                                'trend':['ct', 'c', 't']}}}
+print(default_dict['AutoReg']['params']['lags'])
