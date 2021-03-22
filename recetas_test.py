cbfe55145875d1d8275358215575ad8dbbd362c4
SMG-1 <35125339+SMG-1@users.noreply.github.com>
2021-03-21 22:24:00 -0600

    Update recetas_test.py

--- recetas_test.py
+++ recetas_test.py
@@ -1,15 +1,21 @@
 import pandas as pd
+import os
 
 # leer archivo de ventas
-ventas = pd.read_csv(r"C:\Users\smirand27701\Desktop\Nueva carpeta\Ventas.csv")
+
+path = r'C:\Users\Usuario\Desktop\Data Ticheese'
+
+# leer archivo de ventas
+ventas = pd.read_csv(os.path.join(path, "Ventas.csv"))
 ventas.columns = ['Fecha', 'Cod_Prod', 'Nombre_Prod', 'Cantidad']
+
+# agrupar ventas por fecha - producto
 ventas = ventas.groupby(['Fecha', 'Cod_Prod', 'Nombre_Prod']).sum().reset_index()
 
-# leer Excel de recetas
-recetas = pd.read_excel(r"C:\Users\smirand27701\Desktop\Nueva carpeta\Recetas.xlsx",
+# leer archivo de recetas
+recetas = pd.read_excel(os.path.join(path, "Recetas.xlsx"),
                         sheet_name='OIT RECETAS',
                         header=1)
-# cambiar nombres
 recetas = recetas.loc[:, ['Cod Receta',
                           'Desc Receta',
                           'Cant',
@@ -21,24 +27,39 @@ recetas = recetas.loc[:, ['Cod Receta',
 recetas.columns = ['Cod_Prod', 'Nombre_Prod', 'Cant_Prod', 'Ud_Prod',
                    'Cod_Comp', 'Nombre_Comp', 'Cant_Comp', 'Ud_Comp']
 
-# hacer un dataframe externo con los codigos duplicados de componentes removidos
-test = pd.DataFrame(recetas['Cod_Comp'].drop_duplicates())
+# crear tabla de productos intermedios, primero quitar codigos de componentes duplicados
+intermedios = pd.DataFrame(recetas['Cod_Comp'].drop_duplicates()).dropna()
+intermedios.columns = ['Cod_Inter']
 
-#
-# test = test.merge(recetas[['Cod_Prod', 'Nombre_Prod']], left_on='Cod_Comp',
-test = test.merge(recetas, left_on='Cod_Comp',
-                  right_on='Cod_Prod', how='left')
-test.dropna(subset=['Cod_Prod'], inplace=True)
-test.drop_duplicates(subset=['Cod_Prod'], inplace=True)
+# luego unir con recetas, usando cod intermedio como left key y cod prod como right key
+# se obtienen los productos que son componentes pero tambien tienen sus propios componentes (intermedios)
+intermedios = intermedios.merge(recetas, left_on='Cod_Inter', right_on='Cod_Prod', how='left')
+intermedios.rename(columns={'Nombre_Prod': 'Nombre_Inter'}, inplace=True)
+intermedios.drop(columns=['Cod_Prod'], inplace=True)
+intermedios.dropna(subset=['Cod_Comp'], inplace=True)
 
-recetas_sin_nom = recetas.drop(columns=['Nombre_Prod'])
-ventas_test = ventas.merge(recetas_sin_nom,
+# recetas_sin_nom = recetas.drop(columns=['Nombre_Prod'])
+ventas_mp = ventas.merge(recetas.drop(columns=['Nombre_Prod']),
                            on='Cod_Prod',
                            how='left')
 
+ventas_mp['Cant_Req'] = ventas_mp['Cantidad'] * ventas_mp['Cant_Comp']
 
-ventas_test['Cant_Req'] = ventas_test['Cantidad'] * ventas_test['Cant_Comp']
+ventas_mp = ventas_mp.groupby(['Cod_Comp', 'Nombre_Comp', 'Fecha'])['Cant_Req'].sum().reset_index()
+ventas_mp = ventas_mp[~ventas_mp['Nombre_Comp'].str.contains('RECORTES')]
+ventas_mp.columns = ['Cod_Prod', 'Nombre_Prod', 'Fecha', 'Cant_Req']
+# mp = mp.merge(test, on=)
 
-mp = ventas_test.groupby(['Cod_Comp', 'Nombre_Comp', 'Fecha'])['Cant_Req'].sum().reset_index()
-mp = mp[~mp['Nombre_Comp'].str.contains('RECORTES')]
-mp = mp.merge(test, on=)
+# volver a aplicar recetas para productos intermedios
+ventas_mp_sin_inter = ventas_mp.merge(intermedios, left_on='Cod_Prod', right_on='Cod_Inter', how='left')
+ventas_mp_sin_inter.loc[ventas_mp_sin_inter['Cod_Inter'].notna(), 'Cant_Req_Final'] = ventas_mp_sin_inter['Cant_Req'] * ventas_mp_sin_inter['Cant_Comp']
+ventas_mp_sin_inter.loc[ventas_mp_sin_inter['Cod_Inter'].notna(), 'Codigo'] = ventas_mp_sin_inter['Cod_Comp']
+ventas_mp_sin_inter.loc[ventas_mp_sin_inter['Cod_Inter'].notna(), 'Nombre'] = ventas_mp_sin_inter['Nombre_Comp']
+
+ventas_mp_sin_inter.loc[ventas_mp_sin_inter['Cant_Req_Final'].isna(), 'Codigo'] = ventas_mp_sin_inter['Cod_Prod']
+ventas_mp_sin_inter.loc[ventas_mp_sin_inter['Cant_Req_Final'].isna(), 'Nombre'] = ventas_mp_sin_inter['Nombre_Prod']
+ventas_mp_sin_inter.loc[ventas_mp_sin_inter['Cant_Req_Final'].isna(), 'Cant_Req_Final'] = ventas_mp_sin_inter['Cant_Req']
+
+ventas_mp_sin_inter = ventas_mp_sin_inter[['Codigo', 'Nombre', 'Fecha', 'Cant_Req_Final']]
+ventas_mp_sin_inter.columns = ['Codigo', 'Nombre', 'Fecha', 'Demanda']
+
